17c4776959b4eca67edbd864833b02936ebf7d34
vmnomad <akopbayev@icloud.com>
2019-10-20 15:20:35 +1100

    Twitter bot can retweet and like tweets with a keyword
    First working version of the my first ever twitter bot
    Comes with a preconfigired docker file
    
    Authentication keys and Keyword has to be added to dockerfile
    before building image

---
+++ config.py
@@ -0,0 +1,44 @@
+import tweepy
+import sys
+import logging
+import os
+
+# create logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Check authentication
+def create_api():
+
+    try:
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        access_token = os.getenv("ACCESS_TOKEN")
+        access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+    except:
+        logger.error('Failed to get keys from env variables', exc_info=True)
+        sys.exit('Exiting..')
+
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    except:
+        logger.error('Failed create API', exc_info=True)
+        sys.exit('Exiting..')
+
+    try:
+        api.verify_credentials()
+        logger.info("Authentication OK. API is created")
+        return api
+    except Exception as error:
+        logger.error("Error during authentication: {}".format(error))
+        logger.info('Please ensure the keys.py file is present and contains all keys')
+        sys.exit('Exiting..')
+
+def get_keyword():
+    try:
+        return os.getenv("KEYWORD")
+    except Exception as error:
+        logger.error("Error during authentication: {}".format(error))
+        sys.exit('Exiting..')
--- twitter_bot.py
+++ twitter_bot.py
@@ -1,12 +1,54 @@
 import tweepy
-import keys
+from termcolor import colored
+from config import create_api, get_keyword
+import logging
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
-auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_TOKEN_SECRET)
 
 # Create API object
-api = tweepy.API(auth)
+api = create_api()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Create a tweet
-api.update_status("Hello Tweepy")
+# get keyword
+keyword = get_keyword()
+logger.info('Tracking tweets with keyword: {}'.format(keyword))
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        
+        
+        # ignore own tweet and replies to tweet
+        if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
+            return
+        
+        # check if it is a retweet
+        if hasattr(tweet, 'retweeted_status'):
+            logger.info(colored('This tweet is a retweet. Skipping..', 'yellow'))
+            return
+
+        logger.info((colored('{}'.format(tweet.user.name), 'green') + ': ' + colored('{}'.format(tweet.text), 'blue')))
+        # Like tweet if hasn't been liked yet
+        try:
+            tweet.favorite()
+        except Exception as error:
+            logger.error("Failed to like tweet", exc_info=True)
+
+        # Retweet tweet if hasn't been retweeted yet
+        try:
+            tweet.retweet()
+        except Exception as error:
+            logger.error("Failed to retweet", exc_info=True)
+
+        
+    def on_error(self, status):
+        logger.error("Error detected. Status: {}".format(status))
+
+
+
+tweets_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweets_listener)
+stream.filter(track=[keyword], languages=["en"])
